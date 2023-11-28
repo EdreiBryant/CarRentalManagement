1bc0e0be01a069aca71f21bad0672d9e8d5aefe6
EdreiBryant <2200140H@student.tp.edu.sg>
2023-11-28 11:00:51 +0800

    Week 6 Lab complete

---
+++ Shared/Domain/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRentalManagement.Shared.Domain
+{
+    public class Customer : BaseDomainModel
+    {
+        public string? DrivingLicense { get; set; }
+        public string? Address { get; set;}
+        public string? ContactNumber { get; set;}
+        public string? EmailAddress { get; set;}
+        //public DateTime DateCreated { get; set; }
+        //public DateTime DateUpdated { get; set; }
+        //public string? CreatedBy { get; set; }
+        //public string? UpdatedBy { get; set; }
+        public virtual List<Booking>? Bookings { get; set; }
+    }
+}
